9d2b3862720e38bba0999deb4c093bcd94ec1947
Salman <salmancrc9@gmail.com>
2026-08-11 12:28:32 +0500

    feat: create TasksService with initial mock data and findAll method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [
+    {
+      id: 1,
+      title: 'Learn NestJS',
+      completed: false,
+    },
+  ];
+
+  findAll() {
+    console.log(this);
+    return this.tasks;
+  }
+}
