1ab55f6efbe4681b0a4ec067dd2be371226bbde6
Salman <salmancrc9@gmail.com>
2026-08-21 12:13:56 +0500

    feat: implement PrismaService with PostgreSQL adapter and configure ESLint overrides

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService], //exported, so other modules can use it
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
