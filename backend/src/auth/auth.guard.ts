b5812a523c513601c7018e6101c22531deeaa611
Salman <salmancrc9@gmail.com>
2026-09-02 23:02:23 +0500

    JwtPayload added into interface file for Auth

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -6,11 +6,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-
-interface JwtPayload {
-  sub: number;
-  email: string;
-}
+import { JwtPayload } from './interfaces/jwt.payload.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
