39847e56005e55a8d73f1394f68befc838886cf4
Salman <salmancrc9@gmail.com>
2026-09-10 14:02:31 +0500

    registerAsync added in auth module instead of register for getting JWT Secret

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -8,11 +9,14 @@ import { AuthGuard } from './auth.guard';
 @Module({
   imports: [
     PrismaModule,
-    JwtModule.register({
-      secret: 'my-secret',
-      signOptions: {
-        expiresIn: '1h',
-      },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   controllers: [AuthController],
