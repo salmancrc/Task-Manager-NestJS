70ef2d264f4e03255ff9b044f98e979a431ef5e0
Salman <salmancrc9@gmail.com>
2026-09-11 15:34:58 +0500

    swagger added inside the main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,8 @@ async function bootstrap() {
     credentials: true, // Required for cookies
   });
 
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,6 +25,27 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const document = SwaggerModule.createDocument(app, {
+    openapi: '3.0.0',
+    info: {
+      title: 'Task Manager API',
+      description: 'API documentation for the Task Manager application',
+      version: '1.0',
+    },
+    components: {
+      securitySchemes: {
+        bearer: {
+          type: 'http',
+          scheme: 'bearer',
+        },
+      },
+    },
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
