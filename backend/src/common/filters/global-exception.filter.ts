4602eb185cf6bc80babac8f524c289b3abaa6052
Salman <salmancrc9@gmail.com>
2026-09-11 15:42:44 +0500

    feat: global exception filter added with the response format properly

---
+++ backend/src/common/filters/global-exception.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    let message: string | string[] = 'Internal server error';
+    if (exception instanceof HttpException) {
+      const res = exception.getResponse();
+
+      message =
+        typeof res === 'string'
+          ? res
+          : (res as { message: string | string[] }).message;
+    }
+
+    // Every error response will look exactly like this
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
