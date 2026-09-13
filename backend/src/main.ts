932766a15b5a1edea14d80fbd72c028e95c1981c
Salman <salmancrc9@gmail.com>
2026-09-13 18:18:20 +0500

    localhost 3000 added in enabled CORS origin list

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true, // Required for cookies
   });
 
